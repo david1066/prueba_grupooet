c0341d8470b170d7b98fe3a103a37e28c19d0f4c
david1066 <davinchi1066@hotmail.com>
2022-11-23 15:54:44 -0500

    Relaciones modelos

--- app/Models/Usuario.php
+++ app/Models/Usuario.php
@@ -5,9 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Models\TipoDocumento;
+use App\Models\User;
 class Usuario extends Model
 {
     use HasFactory;
+    //eliminacion por softdeltes
     use SoftDeletes;
     protected $table='usuarios';
     public $fillable = [
@@ -21,4 +24,19 @@ class Usuario extends Model
         'ciudad_id',
         'user_id'
     ];
+    //relacion llave foranea
+    public function TipoDocumento()
+    {
+        return $this->belongsTo(TipoDocumento::class,'tipo_documento_id');
+    }
+    //relacion llave foranea
+    public function Ciudad()
+    {
+        return $this->belongsTo(Ciudad::class,'ciudad_id');
+    }
+    //relacion llave foranea
+    public function User()
+    {
+        return $this->belongsTo(User::class,'user_id');
+    }
 }
--- app/Models/Vehiculo.php
+++ app/Models/Vehiculo.php
@@ -5,6 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Models\Color;
+use App\Models\TipoVehiculo;
+use App\Models\Usuario;
 class Vehiculo extends Model
 {
     use HasFactory;
@@ -16,6 +19,33 @@ class Vehiculo extends Model
         'marca', 
         'tipo_vehiculo_id', 
         'conductor_id',
+        'propietario_id',
         'user_id'
     ];
+
+    //relacion llave foranea
+    public function Color()
+    {
+        return $this->belongsTo(Color::class,'color_id');
+    }
+    //relacion llave foranea
+    public function TipoVehiculo()
+    {
+        return $this->belongsTo(TipoVehiculo::class,'tipo_vehiculo_id');
+    }
+    //relacion llave foranea
+    public function Conductor()
+    {
+        return $this->belongsTo(Usuario::class,'conductor_id');
+    }
+    //relacion llave foranea
+    public function Propietario()
+    {
+        return $this->belongsTo(Usuario::class,'propietario_id');
+    }
+    //relacion llave foranea
+    public function User()
+    {
+        return $this->belongsTo(User::class,'user_id');
+    }
 }
